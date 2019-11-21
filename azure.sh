6ef54b1466f57add26e284d38ae2ec555d195157
Robbie <txt3rob@gmail.com>
2019-11-21 10:43:31 +0000

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -2,4 +2,4 @@
 set -e
 INITIALURL="https://www.microsoft.com/en-in/download/confirmation.aspx?id=41653"
 OUT=$(curl -s "${INITIALURL}" | grep -Eo '"https://[^"]*PublicIPs[^"]*"'|head -n 1|cut -d '"' -f 2)
-curl -s -f "${OUT}" | sed 's|IpRange||g' | sed 's|" />||g' | sed 's|Subnet="||g' | sed 's|<||g' | sed 's| ||g' | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\/[0-9]\{1,2\}\|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
+curl -s -f "${OUT}" | sed 's/[^"]*"\([^"]*\)".*$/\1/g' | grep -Po '(\d+\.?){4}\/\d+'
