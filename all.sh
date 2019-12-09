ddfc4cac9b5368ce35ed90e4bed2b192417dcca6
Filipe Oliveira <285038+filipeoliveiraa@users.noreply.github.com>
2019-12-09 13:25:53 +0000

    add ibm cloud cidr

--- all.sh
+++ all.sh
@@ -7,5 +7,7 @@ echo "[*] Grabbing Google CIDR [*]"
 ./gcloud.sh | grep -F '.' >> tmp.txt
 echo "[*] Grabbing Oracle CIDR [*]"
 ./oracle.sh >> tmp.txt
+echo "[*] Grabbing IBM Cloud CIDR [*]"
+./ibmcloud.sh >> tmp.txt
 cat tmp.txt | sort -u >> all.txt
 echo "[*] Done [*]"
